30533f681d66042b206b54536c18ae411a085ee0
yonghoon304 <142773920+yonghoon304@users.noreply.github.com>
2026-02-12 12:32:06 +0900

    9일차 오전커밋

---
+++ Day09/Chap18-Solution/Chap18-01-app/main.c
@@ -0,0 +1,53 @@
+﻿// 파일 입출력
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <direct.h>
+
+int main(void) {
+	char cwd[256];	// 경로 입력 변수, 윈도우 경로 문자열 최대길이.
+	char str[80];
+	char* res;
+
+	_getcwd(cwd, sizeof(cwd));
+
+	printf("현재 작업 폴더 : %s\n", cwd);
+	
+
+
+
+	// 여기서 부터 텍스트 파일 복사
+	FILE* fp,*ofp; // fp- 파일 입력용 파일 포인터, ofp- 출력용 파일포인터 
+
+	// 디렉토리,폴더 구분자로 /를 사용해도 무방
+	fp = fopen("./data/Sample.txt", "r");
+	if (fp == NULL) {
+		printf("파일 열기 실패\n");
+		return 1;
+	}
+	else {
+		printf("파일 오픈 성공\n");
+	}
+
+	// 파일 복사용 오픈
+	ofp = fopen("./data/copy.txt", "w");
+	if (ofp == NULL) {	// 실패 방지
+		printf("출력파일 열기 실패\n");
+		return 1;
+	}
+
+	while (1) {
+		res = fgets(str, sizeof(str), fp);	// Sample.txt 읽은 파일포인터에서 한줄을 read
+		if (res == NULL) {
+			break;
+		}
+		printf("한줄 쓰기 - > %s\n",str);
+		str[strlen(str) - 1] = '\0';	// \n을 ->\0 바꿔줘서 
+		fputs(str, ofp);
+		fputs(" ", ofp);	// 한줄 끝난뒤 스페이스 추가
+	}
+	printf("텍스트 파일 복사 성공!");
+	fclose(fp);
+	fclose(ofp);
+
+	return 0;
+}
---
+++ Day09/Chap18-Solution/Chap18-02-app/main.c
@@ -0,0 +1,49 @@
+﻿// 파일 입출력 2
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <windows.h>	// OS관련 또는 코드페이지 인코딩 해결
+
+int main(void) {
+	// 콘솔출력,입력을 UTF-8(65001)로 고정설정
+	// SetConsoleOutputCP(65001);	// 콘솔출력 코드페이지
+	// SetConsoleCP(65001);	// 콘솔자체 코드페이지
+	FILE* ifp,*ofp;
+
+	// 텍스트 내 포맷에 맞춰서 변수 선언
+	char name[20];
+	int kor, eng, math;	// 국영수 점수
+	int total;
+	double avg;
+	int res;
+
+	ifp = fopen("./original.txt", "r");	// r모드는 파일 없으면 실패!
+	if (ifp == NULL) {
+		printf("입력파일 오픈 실패!\n");
+		exit(1);
+	}
+	ofp = fopen("./copy.txt", "w");	// w쓰기 모드는 파일이 없으면 파일 생성
+	if (ofp == NULL) {
+		printf("출력파일 생성 실패!\n");
+		exit(1);
+	}
+
+	while (1) {
+		// 입력 파일포인터에서 읽어서 한줄씩 각 변수에 할당
+		res = fscanf(ifp, "%s%d%d%d", name, &kor, &eng, &math);
+		if (res == EOF) {
+			// 파일 데이터의 마지막까지 다 읽으면 EOF
+			break;
+		}
+		total = kor + eng + math;
+		avg = total / 3.0;
+		printf("%s%5d%7.1f\n", name, total, avg);	// 콘솔에 출력
+		
+	}
+	printf("파일 복사 완료\n");
+	//파일 포인터 해제
+	fclose(ifp);
+	fclose(ofp);
+
+	return 0;
+}
